9503300049c36202690e8589f16d48aba19a3e13
Carlos Colina <carlosecolina89@gmail.com>
2024-05-20 11:31:30 -0500

    ocultar botones header

--- resources/views/components/app/header.blade.php
+++ resources/views/components/app/header.blade.php
@@ -1,50 +1,46 @@
 <header class="sticky top-0 bg-white dark:bg-[#182235] border-b border-slate-200 dark:border-slate-700 z-30">
-    <div class="px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-16 -mb-px">
+  <div class="px-4 sm:px-6 lg:px-8">
+    <div class="flex items-center justify-between h-16 -mb-px">
 
-            <!-- Header: Left side -->
-            <div class="flex">
-                
-                <!-- Hamburger button -->
-                <button
-                    class="text-slate-500 hover:text-slate-600 lg:hidden"
-                    @click.stop="sidebarOpen = !sidebarOpen"
-                    aria-controls="sidebar"
-                    :aria-expanded="sidebarOpen"
-                >
-                    <span class="sr-only">Open sidebar</span>
-                    <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <rect x="4" y="5" width="16" height="2" />
-                        <rect x="4" y="11" width="16" height="2" />
-                        <rect x="4" y="17" width="16" height="2" />
-                    </svg>
-                </button>
+      <!-- Header: Left side -->
+      <div class="flex">
 
-            </div>
+        <!-- Hamburger button -->
+        <button class="text-slate-500 hover:text-slate-600 lg:hidden" @click.stop="sidebarOpen = !sidebarOpen"
+          aria-controls="sidebar" :aria-expanded="sidebarOpen">
+          <span class="sr-only">Open sidebar</span>
+          <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <rect x="4" y="5" width="16" height="2" />
+            <rect x="4" y="11" width="16" height="2" />
+            <rect x="4" y="17" width="16" height="2" />
+          </svg>
+        </button>
 
-            <!-- Header: Right side -->
-            <div class="flex items-center space-x-3">
+      </div>
 
-                <!-- Search Button with Modal -->
-                <x-modal-search />
+      <!-- Header: Right side -->
+      <div class="flex items-center space-x-3">
 
-                <!-- Notifications button -->
-                <x-dropdown-notifications align="right" />
+        <!-- Search Button with Modal -->
+        {{-- <x-modal-search /> --}}
 
-                <!-- Info button -->
-                <x-dropdown-help align="right" />
+        <!-- Notifications button -->
+        {{-- <x-dropdown-notifications align="right" /> --}}
 
-                <!-- Dark mode toggle -->
-                <x-theme-toggle />                
+        <!-- Info button -->
+        {{-- <x-dropdown-help align="right" /> --}}
 
-                <!-- Divider -->
-                <hr class="w-px h-6 bg-slate-200 dark:bg-slate-700 border-none" />
+        <!-- Dark mode toggle -->
+        <x-theme-toggle />
 
-                <!-- User button -->
-                <x-dropdown-profile align="right" />
+        <!-- Divider -->
+        <hr class="w-px h-6 bg-slate-200 dark:bg-slate-700 border-none" />
 
-            </div>
+        <!-- User button -->
+        <x-dropdown-profile align="right" />
 
-        </div>
+      </div>
+
     </div>
+  </div>
 </header>
