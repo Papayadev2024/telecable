b2c9f704ddb00d3821eee2a0389427dd5b0e7c8e
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-06-05 14:38:32 -0500

    Agregando titulos a la mayoria de ventanas

--- resources/views/pages/downloader/edit.blade.php
+++ resources/views/pages/downloader/edit.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-app-layout title="Editar Downloader">
 
   <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
     <form action="{{ route('descargables.update', $descargables->id) }}" method="POST" enctype="multipart/form-data">
