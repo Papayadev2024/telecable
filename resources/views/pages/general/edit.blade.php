deb6c01b5346d77e58accefa3798f0863700a4c3
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-09-05 12:03:39 -0500

    final

--- resources/views/pages/general/edit.blade.php
+++ resources/views/pages/general/edit.blade.php
@@ -80,7 +80,30 @@
                                                         </div>
                                                     </div>
 
-                                                    <div class="md:col-span-2">
+                                                    <div class="md:col-span-1">
+                                                        <label for="district">Ciudad</label>
+                                                        <div class="relative mb-2">
+                                                            <div
+                                                                class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                                                    fill="currentColor" viewBox="0 0 20 20"
+                                                                    xmlns="http://www.w3.org/2000/svg">
+                                                                    <path
+                                                                        d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z">
+                                                                    </path>
+                                                                    <path
+                                                                        d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z">
+                                                                    </path>
+                                                                </svg>
+                                                            </div>
+                                                            <input type="text" id="city" name="city"
+                                                                value="{{ $general->city }}"
+                                                                class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                                placeholder="Interior Oficina 204 - Piso 4">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="md:col-span-1">
                                                         <label for="district">Distrito</label>
                                                         <div class="relative mb-2">
                                                             <div
